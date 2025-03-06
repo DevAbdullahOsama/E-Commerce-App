24f2a385d5bc5ed2dde518a1e6fda8b40ed36ec0
Abdullah Osama <abdullah.osama.developer@gmail.com>
2025-03-06 23:21:27 +0200

    intial-commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,11 +3,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'checkout/:id',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Server
   },
   {
     path: 'details/:id',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Server
   },
   {
     path: '**',
